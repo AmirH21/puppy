cea1f93a92c0100f8b4b75d001c0b699b2d93aed
AmirH21 <amirhabsari01@gmail.com>
2024-10-08 15:12:48 +0330

    dog added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AmirH21/puppy
 
 go 1.23.2
+
+require github.com/AmirH21/dog v0.0.0-20241008113535-d0341bf1631f
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/AmirH21/dog"
+)
+
 func Woof() string {
 	return "Woof!"
 }
 
 func Woofs() string {
 	return "Woof Woof Woof!"
+}
+
+func Dog() (string, string) {
+	return dog.Dog(Woof())
+}
+
+func Dogs() (string, string) {
+	return dog.Dog(Woofs())
 }
